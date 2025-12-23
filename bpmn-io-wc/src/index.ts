fe80ad72368635484f7a69e1613f714a9f6809c4
AdrisGithub <adrianjohn6543@gmail.com>
2025-12-23 14:28:20 +0100

    make bpmn-js stylings more readable

--- bpmn-io-wc/src/index.ts
+++ bpmn-io-wc/src/index.ts
@@ -1,7 +1,5 @@
 import { BpmnIOIntegrationElement } from './webcomponent';
-
-import '../../node_modules/bpmn-js/dist/assets/bpmn-js.css';
-import '../../node_modules/bpmn-js/dist/assets/diagram-js.css';
+import './styles.css'
 
 export function registerWebcomponent() {
     if(!customElements.get("bpmn-io-wc")) {

832687b24cb5219e90dde20cf0f7c7d52b17f93c
AdrisGithub <adrianjohn6543@gmail.com>
2025-12-23 17:34:54 +0100

    add activity-status

--- bpmn-io-wc/src/webcomponent.ts
+++ bpmn-io-wc/src/webcomponent.ts
@@ -2,7 +2,8 @@ import {default as BpmnViewerInternal} from 'bpmn-js/lib/NavigatedViewer';
 import {default as OutlineModule} from 'bpmn-js/lib/features/outline/index';
 // @ts-ignore temporary should load the bpmn from remote
 import bpmnText from 'bundle-text:../../examples/test.bpmn';
-import {Canvas} from "bpmn-js/lib/features/context-pad/ContextPadProvider";
+import {default as Canvas} from "diagram-js/lib/core/Canvas";
+import {default as Overlays} from 'diagram-js/lib/features/overlays/Overlays';
 
 export class BpmnIOIntegrationElement extends HTMLElement {
 
@@ -19,9 +20,33 @@ export class BpmnIOIntegrationElement extends HTMLElement {
             width: 1000,
             additionalModules: [OutlineModule]
         })
+        const status = {
+            name: 'Do_Something_Activity',
+            errorCount: 1,
+            runningInstances: 1
+        } as ActivityStatus;
         this.bpmn.importXML(bpmnText)
-            .then(_ => this.bpmn.get<Canvas>('canvas').zoom('fit-viewport'))
+            .then(_ => this.canvas.zoom('fit-viewport'))
+            .then(_ => this.addProcessInstanceOverlays(status))
             .then(value => this.removeBPMNTrademark())
+    }
+
+    addProcessInstanceOverlays(activity: ActivityStatus) {
+        this.overlays.add(activity.name, 'process_instances_indicator', {
+            position: {
+                top: 70,
+                left: 70
+            },
+            html: `<div class="process-instance-count"><p>${activity.runningInstances}</p><p>${activity.errorCount}</p></div>`
+        })
+    }
+
+    get overlays() {
+        return this.bpmn.get<Overlays>("overlays");
+    }
+
+    get canvas() {
+        return this.bpmn.get<Canvas>('canvas');
     }
 
     removeBPMNTrademark() {
@@ -40,6 +65,12 @@ export class BpmnIOIntegrationElement extends HTMLElement {
     disconnectedCallback() {
         this.hookNode.remove()
     }
+}
+
+interface ActivityStatus {
+    name: string,
+    errorCount: number,
+    runningInstances: number,
 }
 
 export const wc_id = "bpmn-io-wc";

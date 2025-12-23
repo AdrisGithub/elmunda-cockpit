29c23574e032a2d1a3fc5f92b127966ca23e8829
AdrisGithub <adrianjohn6543@gmail.com>
2025-12-23 20:17:27 +0100

    refactor index.ts and styles.css

--- bpmn-io-wc/src/index.ts
+++ bpmn-io-wc/src/index.ts
@@ -2,7 +2,7 @@ import {BpmnIOIntegrationElement, wc_id} from './webcomponent';
 import './styles.css'
 
 export function registerWebcomponent() {
-    if(!customElements.get(wc_id)) {
+    if (!customElements.get(wc_id)) {
         customElements.define(wc_id, BpmnIOIntegrationElement)
     }
 }
